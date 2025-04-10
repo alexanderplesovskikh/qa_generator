98aaf4f8157e30a19eb2d7bc21b2555f52a025bd
Плесовских Александр Евгеньевич <aeplesovskikh@edu.hse.ru>
2025-04-10 20:05:09 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,122 +1,110 @@
 
 import zulip
+import os
+import logging
+import re
+import traceback
 
-class GenerateBot:
+user_states = {}
+user_history = {}
 
-    def initialize(self, bot_handler):
-        self.bot_handler = bot_handler
-        self.setup_logging()
-        logging.debug("Инициализация бота")
-        self.user_sessions = {}
+main_menu = "Привет! Я бот-помощник, который умеет генерировать вопросы и ответы для составления экзамена по учебному курсу. Отправь мне ниже файл в формате txt с материалами по ТЕМЕ курса, один файл - одна тема:"
 
-        # Инициализация клиента Zulip
-        self.client = zulip.Client(config_file=os.path.join(os.getcwd(), 'zuliprc'))
+try:
+
+    class GenerateBot:
+
+        def initialize(self, bot_handler):
+            self.bot_handler = bot_handler
+            logging.debug("Инициализация бота")
+            self.user_sessions = {}
+
+            # Инициализация клиента Zulip
+            self.client = zulip.Client(config_file=os.path.join(os.getcwd(), 'genfile/zuliprc'))
+
+        def usage(self):
+            return main_menu
+        
+        
+        def send_reply(self, message, response):
+            response = self.client.send_message({
+                    "type": "private",
+                    "to": message["sender_email"],
+                    "content": response
+                })
+            return response
 
 
-    def handle_message(self, message, bot_handler):
-        user_id = message["sender_id"]
-        content = message["content"].strip().lower()
 
-        if user_id not in user_history:
-            user_history[user_id] = []
+        def handle_message(self, message, bot_handler):
+            user_id = message["sender_id"]
+            content = message["content"].strip().lower()
 
-        if user_id not in user_states:
-            user_states[user_id] = {"state": "main_menu"}
-
-        state = user_states[user_id]["state"]
-
-        if content in ["помощь", 'help', 'start', 'exit']:
-            user_states[user_id] = {"state": "main_menu"}
-            self.send_reply(message, main_menu)
-            return
-
-        if state == "main_menu":
-            if re.match(r"^\d+$", content.strip()) and int(content.strip()) in all_theme_ids:
-                user_states[user_id] = {"state": "select_level", "topic": int(content)}
-                self.send_reply(message, "Какой у вас уровень знаний по теме? Выбери, введя соответствующую цифру: \n**`1. начальный`**\n**`2. средний`**\n**`3. продвинутый`**\n\nЧтобы вернуться в главное меню, введи: **`помощь`**")
-            else:
-                self.send_reply(message, main_menu)
-
-        elif state == "select_level":
-            if content in ["1", "2", "3"]:
-                user_states[user_id] = {"state": "chat", "topic": user_states[user_id]["topic"], "level": content}
-                #open file
-                #prompt model to summorize it
-                topic = user_states[user_id]["topic"]
-
+            if user_id not in user_history:
                 user_history[user_id] = []
 
-                file_path_summary = base_dir + 'course_materials/' + str(topic) + ".txt"
+            if user_id not in user_states:
+                user_states[user_id] = {"state": "main_menu"}
 
-                with open(file_path_summary, 'r', encoding="utf-8") as sum_file:
-                    sum_lines = sum_file.readlines()
+   
 
-                sum_firstlines = sum_lines[0]
+            if content in ["помощь", 'help', 'start', 'exit']:
+                user_states[user_id] = {"state": "main_menu"}
+                self.send_reply(message, main_menu)
+                return
+            
+            if user_states[user_id]["state"] == "main_menu":
 
-                if sum_firstlines[0] == "#":
-                    sum_firstlines = sum_firstlines[1:].strip()
+                self.send_reply(message, main_menu)
+                user_states[user_id] = {"state": "wait_for_file"}
+                return
+            
+            if user_states[user_id]['state'] == 'wait_for_file':
 
-                sum_restlines = " ".join(sum_lines[1:])
+                print(message)
 
-                summary_prompt = f"""- Текст: {sum_restlines}.
-- Задание: ты ИИ-ассистент, напиши короткое резюме данного тебе содержания темы курса, кратко по пунктам обозначь основные разделы, через знак "-".
-- Не пиши много текста, просто приведи подтемы данного тебе материала по курсу, через "-", каждый пункт начинай с новой строки.
-- В конце резюме с нового абзаца, спроси про что-то одно из резюме - задай открытый вопрос по типу: А знаешь ли ты, что такое ..."""
+                if 'content' in message and message['content'] != '':
+                    raw_content = message['content'].strip()
+                    file_pattern = r'^\[.*?\]\(/user_uploads/.*?\)$'
+                    is_only_file_link = bool(re.fullmatch(file_pattern, raw_content))
+                    print(is_only_file_link)
+
+                    self.send_reply(message, f'''File got {is_only_file_link}''')
+
+                '''if 'attachments' in message and message['attachments']:
+                    for attachment in message['attachments']:
+                        if attachment['name'].endswith('.txt'):
+                            # Successful attachment case
+                            user_states[user_id] = {
+                                   "state": "select_level",
+                                   "filename": attachment['name'],
+                                   "file_id": attachment['id']
+                                }
+                            self.send_reply(message, "Какой у вас уровень знаний по теме? Выбери, введя соответствующую цифру: \n**`1. начальный`**\n**`2. средний`**\n**`3. продвинутый`**\n\nЧтобы вернуться в главное меню, введи: **`помощь`**")
+                        else:
+                            self.send_reply(message, "Passing for not txt file")
+                else:
+                    self.send_reply(message, "Пожалуйста, отправьте .txt файл с темой для обсуждения.\n\nЧтобы увидеть главное меню, введи: **`start`**")'''
+                
+            elif user_states[user_id]["state"] == "select_level":
+                if re.match(r"^\d+$", content.strip()):
+                    user_states[user_id] = {"state": "chat", "topic": user_states[user_id]["topic"], "level": content}
+                    topic = user_states[user_id]["topic"]
+                    user_history[user_id] = []
+                    
+                    self.send_reply(message, "👨‍🏫 Давай посмотрим, что нам предстоит узнать в рамках этой темы.")
+                
+                else:
+                    self.send_reply(message, "Пожалуйста, выберите уровень, введя соответствующую цифру: \n**`1. начальный`** \n**`2. средний`** \n**`3. продвинутый`**\n\nЧтобы вернуться в главное меню, введи: **`помощь`**")
+
+            elif user_states[user_id]["state"] == "chat":
+                topic = user_states[user_id]["topic"]
+                level = user_states[user_id]["level"]
 
                 self.send_reply(message, "👨‍🏫 Давай посмотрим, что нам предстоит узнать в рамках этой темы.")
 
-                llm_summary = format_llm_prompt(summary_prompt)
+    handler_class = GenerateBot
 
-                user_states[user_id]['init_message'] = ""
-                user_states[user_id]['last_updated_messsage'] = ""
+except Exception:
+    traceback.print_exc()
 
-
-                user_states[user_id]['init_message'] = f"Проведу тебе краткий экскурс по теме: **{sum_firstlines}**\n---\n"
-
-                event_id = self.send_reply(message, user_states[user_id]['init_message'])
-
-                if event_id:
-                    count = 0
-                    for token in llm_summary:
-                        user_states[user_id]['init_message'] += token
-                        count+= 1 
-                        if count % token_speed == 0:
-                            self.update_reply(message, user_states[user_id]['init_message'], event_id)
-                            user_states[user_id]['init_message'] = user_states[user_id]['init_message']
-                            
-                    
-                    if user_states[user_id]['init_message'] != user_states[user_id]['last_updated_messsage']:
-                        self.update_reply(message, user_states[user_id]['init_message'], event_id)
-
-                    ending_message = "\n---\n**❓ Давай начнём изучение темы! Можешь задать вопрос по любому аспекту темы, я отвечу с опорй на материалы курса Сетевых видеотехнологий или можем начать беседу по предложенному мной вопросу — в любом случае выбирать тебе! Просто `напиши свой вопрос` или отправь мне: `Расскажи, что такое <...>` — и мы начнем беседу 😉**\n---\nДля возврата в меню введите **`помощь`**."
-                    self.update_reply(message, user_states[user_id]['init_message'] + ending_message, event_id)
-
-                    if len(user_history[user_id]) > 10:
-                        del user_history[user_id][0]
-
-                    user_history[user_id].append({"role": "AI-assistant", "message": user_states[user_id]['init_message']})
-            
-            else:
-                self.send_reply(message, "Пожалуйста, выберите уровень, введя соответствующую цифру: \n**`1. начальный`** \n**`2. средний`** \n**`3. продвинутый`**\n\nЧтобы вернуться в главное меню, введи: **`помощь`**")
-
-        elif state == "chat":
-            topic = user_states[user_id]["topic"]
-            level = user_states[user_id]["level"]
-            topic_name = lines[int(str(topic).strip())-1]
-
-            model_level_desc = ''
-            if str(level).strip() == '1':
-                model_level_desc = 'объяснения попроще с простой лексикой и покроче'
-            if str(level).strip() == '2':
-                model_level_desc = 'средний уровень сложности лексики и средний уровень сложности объяснений'
-            if str(level).strip() == '3':
-                model_level_desc = 'сложный уровень лексики научный академический формальный язык и продвинутые объяснения'
-
-            #Main answer
-
-            chunks_for_llm = search_chunks(content)
-
-           
-
-
-handler_class = GenerateBot
